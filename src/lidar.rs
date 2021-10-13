a668300808a2714264043b0b7c713d9660ddbabd
YdrMaster <ydrml@hotmail.com>
2021-10-13 17:44:51 +0800

    fix: 联入系统

--- src/lidar.rs
+++ src/lidar.rs
@@ -8,7 +8,9 @@ use lidar_faselase::{
 };
 use pm1_sdk::model::{Odometry, Physical};
 use std::{
+    f32::consts::FRAC_PI_8,
     f64::consts::PI,
+    io::Write,
     net::{IpAddr, SocketAddr, UdpSocket},
     sync::mpsc::{Receiver, Sender},
     thread,
@@ -21,30 +23,30 @@ pub(super) fn supervisor(chassis: Sender<MsgToChassis>, mail_box: Receiver<MsgTo
     let mut indexer = Indexer::new(2);
     let mut frame = [
         FrameCollector {
-            trans: (118, 0, 0.0),
+            trans: (-141, 0, PI),
             ..FrameCollector::new()
         },
         FrameCollector {
-            trans: (-141, 0, PI),
+            trans: (118, 0, 0.0),
             ..FrameCollector::new()
         },
     ];
     const FILTERS: [fn(Point) -> bool; 2] = [
         |Point { len: _, dir }| {
-            const LIMIT: u16 = 1375; // 5760 * 1.5 / 2π
-            dir < LIMIT || (5760 - LIMIT) <= dir
-        },
-        |Point { len: _, dir }| {
             const DEG90: u16 = 5760 / 4;
             const DEG30: u16 = DEG90 / 3;
             (DEG30 < dir && dir <= DEG90) || ((5760 - DEG90) < dir && dir <= (5760 - DEG30))
+        },
+        |Point { len: _, dir }| {
+            const LIMIT: u16 = 1375; // 5760 * 1.5 / 2π
+            dir < LIMIT || (5760 - LIMIT) <= dir
         },
     ];
 
     let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
     let mut address = None;
     let mut send_time = Instant::now() + Duration::from_millis(100);
-    let mut update_filter = false;
+    let mut update_filter = 2;
 
     SupervisorForMultiple::<D10>::new().join(2, |e| {
         match e {
@@ -53,7 +55,7 @@ pub(super) fn supervisor(chassis: Sender<MsgToChassis>, mail_box: Receiver<MsgTo
                 if let Some(i) = indexer.add(k.clone()) {
                     lidar.send(FILTERS[i]);
                     if i == 0 {
-                        update_filter = true;
+                        update_filter = 1;
                     }
                     for c in &mut frame[i..] {
                         c.clear();
@@ -64,7 +66,7 @@ pub(super) fn supervisor(chassis: Sender<MsgToChassis>, mail_box: Receiver<MsgTo
                 eprintln!("disconnected: COM{}", k);
                 if let Some(i) = indexer.remove(k) {
                     if i == 0 {
-                        update_filter = true;
+                        update_filter = 0;
                     }
                     for c in &mut frame[i..] {
                         c.clear();
@@ -75,9 +77,9 @@ pub(super) fn supervisor(chassis: Sender<MsgToChassis>, mail_box: Receiver<MsgTo
                 let now = Instant::now();
                 // 更新
                 if let Some(j) = indexer.find(&k) {
-                    if j == 1 && update_filter {
-                        update_filter = false;
-                        let _ = s.send(FILTERS[1]);
+                    if j == update_filter {
+                        update_filter = 2;
+                        let _ = s.send(FILTERS[j]);
                     }
                     if let Some((_, (i, s))) = e {
                         frame[j].put(i as usize, s);
@@ -98,6 +100,7 @@ pub(super) fn supervisor(chassis: Sender<MsgToChassis>, mail_box: Receiver<MsgTo
                                 time += PERIOD;
                                 if time > Duration::from_secs(2) {
                                     let _ = chassis.send(MsgToChassis::Move(target));
+                                    println!("! 127");
                                     break;
                                 }
                                 let delta = model.physical_to_odometry(Physical {
@@ -107,18 +110,19 @@ pub(super) fn supervisor(chassis: Sender<MsgToChassis>, mail_box: Receiver<MsgTo
                                 size += delta.s;
                                 pose += delta;
                                 if collision::detect(&frame, pose.pose, size) {
-                                    if time > Duration::from_millis(300) {
-                                        let k = time.as_secs_f32() / 2.0;
+                                    let k = time.as_secs_f32() / 2.0;
+                                    if pose.s > 0.15 || pose.a > FRAC_PI_8 {
                                         let _ = chassis.send(MsgToChassis::Move(Physical {
                                             speed: target.speed * k,
                                             ..target
                                         }));
                                     }
+                                    println!("! {}", (k * 25.5) as u8);
                                     break;
                                 }
                             }
                         }
-                        Send(a) => address = a.map(|a| SocketAddr::new(IpAddr::V4(a), 5005)),
+                        Send(a) => address = a.map(|a| SocketAddr::new(IpAddr::V4(a), 50005)),
                     }
                 }
                 // 发送
@@ -126,9 +130,10 @@ pub(super) fn supervisor(chassis: Sender<MsgToChassis>, mail_box: Receiver<MsgTo
                     if now >= send_time {
                         send_time = now + Duration::from_millis(100);
                         let mut buf = Vec::new();
-                        frame[0].write_to(&mut buf);
+                        let _ = buf.write_all(&[255]);
                         frame[1].write_to(&mut buf);
-                        eprintln!("{:?}", socket.send_to(buf.as_slice(), a));
+                        frame[0].write_to(&mut buf);
+                        let _ = socket.send_to(buf.as_slice(), a);
                     }
                 }
             }
