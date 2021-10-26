0afc743d3b52f0f7ed924ff4d7e506ca0fff3ed8
YdrMaster <ydrml@hotmail.com>
2021-10-26 15:15:54 +0800

    cleanup: delete code useless

---
+++ src/bin/example.rs
@@ -0,0 +1,16 @@
+﻿use robot_bin::Robot;
+
+#[async_std::main]
+async fn main() {
+    let (_robot, events) = Robot::spawn(false).await;
+    while let Ok(e) = events.recv().await {
+        use robot_bin::Event::*;
+        match e {
+            ChassisStatusUpdated(_) => {}
+            ChassisOdometerUpdated(_, _) => {}
+            PoseUpdated(_, _) => {}
+            LidarFrameEncoded(_) => {}
+            CollisionDetected(_) => {}
+        }
+    }
+}
